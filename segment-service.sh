cfd0cb716bb44af55e5b9e01b67e850b31188862
yanai <yanai@tikalk.com>
2015-12-03 10:49:38 +0200

    runtime

--- segment-service.sh
+++ segment-service.sh
@@ -6,4 +6,4 @@ DIRNAME=`dirname $0`
 PROJ_HOME=`cd $DIRNAME/.;pwd;`
 export PROJ_HOME;
 
-java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4jLogDelegateFactory -Dlog4j.configuration=file:$PROJ_HOME/conf/log4j.properties -jar $PROJ_HOME/build/libs/as-segment-service-3.1.0-fat.jar -ha -conf $PROJ_HOME/conf/conf.json
+java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4jLogDelegateFactory -Dlog4j.configuration=file:$PROJ_HOME/conf/log4j.properties -jar $PROJ_HOME/build/libs/as-segments-service-3.1.0-fat.jar -conf $PROJ_HOME/conf/conf.json
